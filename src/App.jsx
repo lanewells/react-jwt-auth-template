f73a2d1df96ef51415cb3568e781ea928345d8f9
lanewells <wellsdelaneyk@gmail.com>
2024-11-13 11:12:21 -0800

    Add Sign Up function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-// src/App.jsx
-
 import { useState } from "react"
 import { Routes, Route } from "react-router-dom"
 import NavBar from "./components/NavBar/NavBar"
+import Landing from "./components/Landing/Landing"
+import Dashboard from "./components/Dashboard/Dashboard"
+import SignupForm from "./components/SignupForm/SignupForm" // import the SignupForm
+const BACKEND_URL = import.meta.env.VITE_EXPRESS_BACKEND_URL
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -10,6 +12,14 @@ const App = () => {
   return (
     <>
       <NavBar user={user} />
+      <Routes>
+        {user ? (
+          <Route path="/" element={<Dashboard user={user} />} />
+        ) : (
+          <Route path="/" element={<Landing />} />
+        )}
+        <Route path="/signup" element={<SignupForm setUser={setUser} />} />
+      </Routes>
       <h1>Hello world!</h1>
     </>
   )
